e52bd872b2469aede756dce170b0d37ecd93aee5
zijunn93 <34432534+zijunn93@users.noreply.github.com>
2018-11-21 17:56:13 +0800

    charts

--- app/components/PieCharts/index.js
+++ app/components/PieCharts/index.js
@@ -25,6 +25,19 @@ const tooltipItemStyle = {
 
 /* eslint-disable react/prefer-stateless-function */
 class PieCharts extends React.PureComponent {
+  renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const RADIAN = Math.PI / 180;
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x  = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+    
+    return (
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  }
+
   render() {
     const { data } = this.props;
     if (!data || data.length < 1) {
@@ -42,6 +55,7 @@ class PieCharts extends React.PureComponent {
           innerRadius={60}
           outerRadius={120}
           strokeWidth={0}
+          // label={this.renderCustomizedLabel}
         >
           {data.map(({ name }, index) => (
             <Cell
